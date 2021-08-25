f27460246d6c6fc96a5c0045e2b8e9adc9bf188f
Osama Baker <edewanjy@hotmail.com>
2021-08-25 03:36:56 +0300

    added sequelize for heroku

--- index.js
+++ index.js
@@ -3,4 +3,14 @@
 const server = require('./server')
 require('dotenv').config();
 
+let sequelizeOptions = {
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      }
+};
+let sequelize = new Sequelize(POSTGRES_URI,sequelizeOptions);
+
 server.start(process.env.PORT)
